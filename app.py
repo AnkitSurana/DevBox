baf6b96fa178320b0775b1bdb2d2a64ee09802b8
AnkitSurana <ankitsurana002@gmail.com>
2024-05-27 17:27:29 +0530

    Working Demo

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-import requests
+import os
 import subprocess
-import webbrowser
-from flask import Flask, render_template 
+import socket
+import requests
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__, static_url_path='/static')
+
+# Function to get the EC2 instance's private IP using socket
+def get_instance_private_ip():
+    try:
+        hostname = socket.gethostname()
+        ip_address = socket.gethostbyname(hostname)
+        return ip_address
+    except socket.error as e:
+        app.logger.error(f"Error fetching instance private IP: {e}")
+        return None
 
 # Route to render the index.html file
 @app.route('/')
@@ -13,51 +24,43 @@ def index():
 # Function to check if a URL is reachable
 def is_url_reachable(url):
     try:
-        response = requests.get(url)
+        response = requests.get(url, timeout=2)
         return response.status_code == 200
-    except requests.exceptions.ConnectionError:
+    except requests.exceptions.RequestException as e:
+        app.logger.error(f"Error reaching URL {url}: {e}")
         return False
 
 # Route to launch Airflow based on the selected environment
 @app.route('/launch_airflow/<environment>')
 def launch_airflow(environment):
     airflow_url = f'https://airflow-master.{environment}.assistant.natwest.com/'  # Replace with your actual Airflow URL
-    # if is_url_reachable(airflow_url):
-    #     # Open Airflow in a new browser tab
-    webbrowser.open_new_tab(airflow_url)
-    #     print(f'Airflow for environment {environment} is already accessible')
-    # else:
-    #     print(f'Airflow for environment {environment} is not reachable', 404)
+    return jsonify({'url': airflow_url})
 
-
-# Route to launch a insight portal in a new tab
+# Route to launch an insight portal in a new tab
 @app.route('/launch_insight_portal')
 def launch_insight_portal():
     url = 'http://example.com/form'  # Replace with your actual URL
-    # if is_url_reachable(url):
-        # Open the insight portal in a new browser tab
-    webbrowser.open_new_tab(url)
-        # print(f'Insight Portal is already accessible')
-    # else:
-    #     print(f'Insight Portal is not reachable', 404)
-
+    return jsonify({'url': url})
 
 # Route to launch Jupyter Lab or open the browser
 @app.route('/launch_jupyter_lab')
 def launch_jupyter_lab():
-    url = 'http://localhost:8888/lab'
+    instance_ip = get_instance_private_ip()
+    if not instance_ip:
+        return jsonify({'error': 'Could not determine instance IP address.'}), 500
+
+    url = f'http://{instance_ip}:8888/lab'
     if is_url_reachable(url):
-        # Open Jupyter Lab in a new browser tab
-        webbrowser.open_new_tab(url)
-        print(f'Jupyter Lab is already running on port 8888')
+        return jsonify({'url': url})
     else:
         try:
             # Run Jupyter Lab command as a subprocess with port 8888 and ignore existing processes on the same port
-            subprocess.Popen(['jupyter', 'lab', '--port', '8888', '--port-retries', '0'])
-            print(f'Jupyter Lab launched successfully!')
+            subprocess.Popen(['jupyter', 'lab', '--ip', '0.0.0.0', '--port', '8888', '--port-retries', '0'])
+            return jsonify({'url': url})
         except Exception as e:
-            print(f'Error launching Jupyter Lab: {e}', 500)
-
+            app.logger.error(f"Error launching Jupyter Lab: {e}")
+            return jsonify({'error': f'Error launching Jupyter Lab: {e}'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
